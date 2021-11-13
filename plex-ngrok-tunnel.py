3f11b6b51b319048c1dbcd899358bf1d1f4d3b34
therithwikrayani <therithwikrayani@gmail.com>
2021-11-13 12:25:03 +0530

    Fix to work with TCP tunnel again

--- plex-ngrok-tunnel.py
+++ plex-ngrok-tunnel.py
@@ -5,19 +5,20 @@ from plexapi.server import PlexServer
 TOKEN = '<your-token-here>' # Make sure token is in quotes
 PUBLIC_PORT = 32400 # 32400 is the default port for Plex, do not change unless you know what you're doing.
 
-while True:        
+while True:
     # Open ngrok tunnel
     tunnel = ngrok.connect(PUBLIC_PORT, "tcp")
+    public_url = "https" + tunnel.public_url[3:]
     print('ngrok tunnel opened')
 
     # Set Plex Custom URL
-    baseurl = 'http://localhost:' + str(PUBLIC_PORT)
-    plex = PlexServer(baseurl, TOKEN)
-    plex.settings.get('customConnections').set(tunnel.public_url)
+    private_url = 'http://localhost:' + str(PUBLIC_PORT)
+    plex = PlexServer(private_url, TOKEN)
+    plex.settings.get('customConnections').set(public_url)
     plex.settings.save()
     print('Plex custom URL set')
 
     time.sleep(4 * 3600) # Keep tunnel online, restart tunnel every 4 hours
 
     print('ngrok tunnel restarting')
-    ngrok.disconnect(tunnel.public_url)
+    ngrok.disconnect(public_url)
