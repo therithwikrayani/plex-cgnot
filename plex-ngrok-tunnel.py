33cead40a6e9108248a9004bfb9e2035b64f7779
Rithwik Rayani <therithwikrayani@gmail.com>
2022-02-19 10:06:54 +0530

    Removed periodic tunnel refresh, as it serves no purpose. ngrok restarts the tunnel overnight.

--- plex-ngrok-tunnel.py
+++ plex-ngrok-tunnel.py
@@ -6,23 +6,20 @@ from plexapi.server import PlexServer
 TOKEN = '<your-token-here>' # Make sure token is in quotes
 PUBLIC_PORT = 32400 # 32400 is the default port for Plex, do not change unless you know what you're doing.
 
+# Open ngrok tunnel
+tunnel = ngrok.connect(PUBLIC_PORT, "tcp")              # tunnel = tcp://abc:xyz
+url = tunnel.public_url[6:].split(":")[0]               # url = abc
+ip = socket.gethostbyname(url)                          # ip = gethostbyname(abc) = 123
+port = tunnel.public_url[6:].split(":")[1]              # port = xyz
+public_url = "https://" + ip + ":" + port               # public_url = https://123:xyz
+print('ngrok tunnel opened')
+
+# Set Plex Custom URL
+private_url = 'http://localhost:' + str(PUBLIC_PORT)
+plex = PlexServer(private_url, TOKEN)
+plex.settings.get('customConnections').set(public_url)
+plex.settings.save()
+print('Plex custom URL set')
+
 while True:
-    # Open ngrok tunnel
-    tunnel = ngrok.connect(PUBLIC_PORT, "tcp")              # tunnel = tcp://abc:xyz
-    url = tunnel.public_url[6:].split(":")[0]               # url = abc
-    ip = socket.gethostbyname(url)                          # ip = gethostbyname(abc) = 123
-    port = tunnel.public_url[6:].split(":")[1]              # port = xyz
-    public_url = "https://" + ip + ":" + port               # public_url = https://123:xyz
-    print('ngrok tunnel opened')
-
-    # Set Plex Custom URL
-    private_url = 'http://localhost:' + str(PUBLIC_PORT)
-    plex = PlexServer(private_url, TOKEN)
-    plex.settings.get('customConnections').set(public_url)
-    plex.settings.save()
-    print('Plex custom URL set')
-
-    time.sleep(12 * 3600) # Keep tunnel online, restart tunnel every 12 hours
-
-    print('ngrok tunnel restarting')
-    ngrok.disconnect(public_url)
+    time.sleep(3600)
